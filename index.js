52847a91155d86dda66602c45ae7b4523af3da92
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-23 17:34:15 -0800

    Route returns json of persons

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('hello world');
+const persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  }
+];
+
+app.get('/api/persons', function (req, res) {
+  res.json(persons);
 });
 
 app.listen(3001);
